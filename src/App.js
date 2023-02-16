f559fb16297d71d3f034b67bcfe06a72242fabdb
icarodolzane <icarodolzane@live.com>
2023-02-16 15:17:17 -0400

    Adicionado login-project-screenshot

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import gfd_screenshot from "./images/gfd-site.png"
+import login_screenshot from "./images/login.png"
 function App() {
   return (
     <div className="App">
       <img src={ gfd_screenshot } alt="screenshot-gfd"/>
+      <img src={ login_screenshot } alt="screenshot-login"/>
     </div>
   );
 }
