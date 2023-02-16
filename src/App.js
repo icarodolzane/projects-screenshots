5b3ea1641db85179221b0ad4daecf061d9b50b7b
icarodolzane <icarodolzane@live.com>
2023-02-16 00:15:06 -0400

    Adicionado screenshot 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import gfd_screenshot from "./images/gfd-site.png"
+function App() {
+  return (
+    <div className="App">
+      <img src={ gfd_screenshot } alt="screenshot-gfd"/>
+    </div>
+  );
+}
+
+export default App;
